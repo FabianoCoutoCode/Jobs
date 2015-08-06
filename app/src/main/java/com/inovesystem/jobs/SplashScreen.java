5f8828f381ea0a5ba69193adf36cf40aac5487a2
FabianoC <fc_sergipe@hotmail.com>
2015-08-06 20:51:35 -0300

    1-Splash-ok 2-Menu-ok

---
+++ app/src/main/java/com/inovesystem/jobs/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.inovesystem.jobs;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by vamsikrishna on 12-Feb-15.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(2000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent i = new Intent("com.inovesystem.jobs.MAINACTIVITY");
+                    startActivity(i);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
